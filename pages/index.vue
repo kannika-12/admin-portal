5e8537633c209919ae562c07c7a1e05a3ab4e7ee
Mew-Trainee <takecare.trainee.01@gmail.com>
2025-07-07 16:43:56 +0700

    ติดตั้งTailwindcss

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <h1>Page Detail</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus,
+      doloribus.
+    </p>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped></style>
